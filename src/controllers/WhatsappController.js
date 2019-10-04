c17f7ce4d6186855ba99b7de82a65dc90209b9ad
diogoguedes <diogo.pereira@4all.com>
2019-10-04 16:31:52 -0300

    ajuste de envio do whatsapp quando tiver mais de um resultado do colaborador

--- src/controllers/WhatsappController.js
+++ src/controllers/WhatsappController.js
@@ -3,29 +3,43 @@ const authToken = '1e6dc24e9b8a3555e7012817d5337cb1';
 const client = require('twilio')(accountSid, authToken);
 const Collaborator = require('../models/Collaborator')
 
-module.exports = {    
-   async notifyByWhats(req, res){
-        const {collaboratorName, message}=req.body
-        const collaborator = await Collaborator.findOne({name:collaboratorName})
-            if(collaborator){
-                const {name, phone, number} = collaborator
+module.exports = {
+    async notifyByWhats(req, res) {
+        const { collaboratorName, message } = req.body
+        let collaborator = await Collaborator.find(({ name: { $regex: new RegExp(collaboratorName) } }));
+        if (collaborator.length <= 1) {
+            //const { name, phone } = collaborator[0]
+            collaborator.map(data => {
                 client.messages.create({
                     from: 'whatsapp:+14155238886', // From a valid Twilio number
-                    to: 'whatsapp:+55' + phone,  // Text this number
-                    body: 'olá ' + name.toUpperCase() +' '+ message,
+                    to: 'whatsapp:+55' + data.phone,  // Text this number
+                    body: 'olá ' + data.name.toUpperCase() + ' ' + message,
                 })
-                .then((result) => {
-                    return res.json({
-                        result:true,
-                        message:result
-                    });
-                }).catch((error) => {
-                    console.log('erro =>',error)
-                     return res.json({
-                         result:false,
-                         errorMessage: error
+                    .then((result) => {
+                        return res.json({
+                            result: true,
+                            type: 'success',
+                            message: result
                         });
-                })
-            }
+                    }).catch((error) => {
+                        console.log('erro =>', error)
+                        return res.json({
+                            result: false,
+                            type: 'error',
+                            message: error
+                        });
+                    })
+            })
+        } else {
+            let collaboratorfinds = []
+            collaborator.map(data => {
+                collaboratorfinds.push(data.name)
+            })
+            return res.json({
+                result: true,
+                type: 'doubt',
+                message: collaboratorfinds
+            })
+        }
     },
 }
